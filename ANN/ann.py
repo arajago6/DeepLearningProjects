95d9f12e136278d610c0ac5a36c5d36d62762e84
arajago6 <arajago6@hawk.iit.edu>
2017-05-19 00:00:12 -0500

    Split and scale train test sets

--- ANN/ann.py
+++ ANN/ann.py
@@ -33,3 +33,13 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 # Remove first column to avoid dummy variable trap
 X = X[:,1:]
+
+# Splitting the dataset into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
