4c136ee09d37c465d69a350a8cd27c0f8245f5b6
arajago6 <arajago6@hawk.iit.edu>
2017-05-20 10:17:05 -0500

    Build CNN for image classification

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -20,3 +20,19 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+# Initializing the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution with 64 3X3 filters and unit strides on color images resized to 64X64
+classifier.add(Convolution2D(32, (3,3), input_shape = (64,64,3), activation = 'relu'))
+
+# Step 2 - 2X2 Max Pooling. Strides is same as pool_size. 
+classifier.add(MaxPooling2D(pool_size = (2,2), strides = (2,2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full Connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
