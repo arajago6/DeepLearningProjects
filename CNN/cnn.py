f05b52ff0648d86c34543ac525a1e20547c81350
arajago6 <arajago6@hawk.iit.edu>
2017-05-20 11:07:54 -0500

    Compile CNN and start fitting process

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -36,3 +36,11 @@ classifier.add(Flatten())
 # Step 4 - Full Connection
 classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+from keras.preprocessing.image import ImageDataGenerator
+
+
