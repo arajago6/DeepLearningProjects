e1f573f8063bfcb1b71f52da339f1fcfba1bdda7
arajago6 <arajago6@hawk.iit.edu>
2017-05-21 19:37:17 -0500

    Start building an RNN to predict Google stock prices

---
+++ RNN/rnn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 21 19:01:40 2017 using Sypder
+
+@author: arajago6
+
+Build a Recurrent Neural Network to predcit stock prices of Google for a month, 
+having trained the RNN using 5 years of historic stock price data
+
+"""
+
+# Part 1 - Data preprocessing
+
+# Importing the needed libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
