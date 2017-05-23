d05c17e1c6b573ff45dd6ad34101ab6d31c5488e
arajago6 <arajago6@hawk.iit.edu>
2017-05-23 13:40:01 -0500

    Build RNN architecture and compile it

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -33,3 +33,22 @@ y_train = training_set[1:num_observations]
 
 # Reshape inputs to match expectation of keras recurrent layer
 X_train = np.reshape(X_train,(num_observations-1,1,1))
+
+# Part 2 - Building the RNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+# Initializing the RNN
+regressor = Sequential()
+
+# Adding the input layer and LSTM layer
+regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 1)))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compile the RNN
+regressor.compile(optimizer = 'rmsprop', loss = 'mean_squared_error')
