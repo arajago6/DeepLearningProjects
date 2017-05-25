76456c6c7a0cf0557e626f54cd717b464400ca89
arajago6 <arajago6@hawk.iit.edu>
2017-05-25 12:54:31 -0500

    Initialize and train Self Organizing Map

--- SelfOrganizingMaps/som.py
+++ SelfOrganizingMaps/som.py
@@ -6,6 +6,7 @@ Created on Wed May 24 17:41:14 2017 using Spyder
 @author: arajago6
 
 Build a Self Organizing Map to predict acceptance for credit card applications (Statlog UCI dataset)
+Uses MiniSom 1.0 package by Giuseppe Vettigli. Liscense: https://creativecommons.org/licenses/by/3.0/
 
 """
 
@@ -23,3 +24,10 @@ y = dataset.iloc[:, -1].values
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 X = sc.fit_transform(X)
+
+# Training the SOM
+from minisom import MiniSom
+# We map our 15 feature records into a 10X10 grid
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
